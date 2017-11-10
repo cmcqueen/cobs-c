da2553c89fd37907782a1af82bc0b8024a400570
Craig McQueen <craig.mcqueen@innerrange.com>
2017-11-09 20:44:21 -0800

    Rename Python unit tests, and remove build scripts

--- UnitTestPython/build-win-dll.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem gcc -c -fPIC -I.. ../cobs.c -o libcobs.o
-rem gcc -shared libcobs.o -o libcobs.dll
-
-gcc -shared -I.. ../cobs.c -o libcobs.dll
-gcc -shared -I.. ../cobsr.c -o libcobsr.dll
