fb9df26dcebe1813fb501c093f3b18edeb5905e6
Craig McQueen <craig.mcqueen@innerrange.com>
2023-12-25 16:29:25 +1100

    Improvements to COBS unit tests

--- tests-c/test-cobs.c
+++ tests-c/test-cobs.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
  *
- * \file
+ * \file test-cobs.c
  *
  * \brief Unit Tests for COBS
  *
@@ -143,14 +143,14 @@ void test_cobs_fixed(void)
         memset(encode_out, 'A', sizeof(encode_out));
 
         encode_result = cobs_encode(encode_out, sizeof(encode_out), cobs_tests[i].data_ptr, cobs_tests[i].data_len);
-        TEST_ASSERT_EQUAL_size_t(cobs_tests[i].encoded_len, encode_result.out_len);
-        TEST_ASSERT_EQUAL_UINT8_ARRAY(cobs_tests[i].encoded_ptr, encode_out, cobs_tests[i].encoded_len);
+        TEST_ASSERT_EQUAL_size_t_MESSAGE(cobs_tests[i].encoded_len, encode_result.out_len, cobs_tests[i].description_ptr);
+        TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(cobs_tests[i].encoded_ptr, encode_out, cobs_tests[i].encoded_len, cobs_tests[i].description_ptr);
 
         decode_result = cobs_decode(decode_out, sizeof(decode_out), encode_out, encode_result.out_len);
-        TEST_ASSERT_EQUAL_size_t(cobs_tests[i].data_len, decode_result.out_len);
+        TEST_ASSERT_EQUAL_size_t_MESSAGE(cobs_tests[i].data_len, decode_result.out_len, cobs_tests[i].description_ptr);
         if (cobs_tests[i].data_len)
         {
-            TEST_ASSERT_EQUAL_UINT8_ARRAY(cobs_tests[i].data_ptr, decode_out, cobs_tests[i].data_len);
+            TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(cobs_tests[i].data_ptr, decode_out, cobs_tests[i].data_len, cobs_tests[i].description_ptr);
         }
     }
 }
@@ -158,7 +158,7 @@ void test_cobs_fixed(void)
 void test_cobs_zeros(void)
 {
     uint8_t             data[530];
-    uint8_t             expected_encode[530];
+    uint8_t             expected_encode[COBS_ENCODE_DST_BUF_LEN_MAX(530)];
     size_t              expected_encode_len;
     uint8_t             encode_out[COBS_ENCODE_DST_BUF_LEN_MAX(530)];
     uint8_t             decode_out[530];
@@ -182,11 +182,53 @@ void test_cobs_zeros(void)
     }
 }
 
+void test_cobs_255(void)
+{
+    uint8_t *           p_dst;
+    uint8_t             data[530];
+    uint8_t             expected_encode[COBS_ENCODE_DST_BUF_LEN_MAX(530)];
+    size_t              expected_encode_len;
+    uint8_t             encode_out[COBS_ENCODE_DST_BUF_LEN_MAX(530)];
+    uint8_t             decode_out[530];
+    char                msg[100];
+    cobs_encode_result  encode_result;
+    cobs_decode_result  decode_result;
+    size_t              i;
+    size_t              chunk_len;
+    size_t              j;
+
+    memset(data, 0xFFu, sizeof(data));
+    for (i = 1u; i < dimof(data); i++)
+    {
+        snprintf(msg, sizeof(msg), "i = %zu", i);
+        p_dst = expected_encode;
+        expected_encode_len = 0;
+        for (j = 0, chunk_len = 0;
+            j < i;
+            j += chunk_len)
+        {
+            chunk_len = MIN(254, i - j);
+            *p_dst++ = (uint8_t)(chunk_len + 1u);
+            memset(p_dst, 0xFFu, chunk_len);
+            p_dst += chunk_len;
+            expected_encode_len += chunk_len + 1u;
+        }
+
+        encode_result = cobs_encode(encode_out, sizeof(encode_out), data, i);
+        TEST_ASSERT_EQUAL_size_t_MESSAGE(expected_encode_len, encode_result.out_len, msg);
+        TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected_encode, encode_out, encode_result.out_len, msg);
+
+        decode_result = cobs_decode(decode_out, sizeof(decode_out), encode_out, encode_result.out_len);
+        TEST_ASSERT_EQUAL_size_t_MESSAGE(i, decode_result.out_len, msg);
+        TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(data, decode_out, i, msg);
+    }
+}
+
 void test_cobs_nonzeros(void)
 {
     uint8_t *           p_dst;
     uint8_t             data[530];
-    uint8_t             expected_encode[530];
+    uint8_t             expected_encode[COBS_ENCODE_DST_BUF_LEN_MAX(530)];
     size_t              expected_encode_len;
     uint8_t             encode_out[COBS_ENCODE_DST_BUF_LEN_MAX(530)];
     uint8_t             decode_out[530];
@@ -249,6 +291,7 @@ int main(int argc, char **argv)
 
     RUN_TEST(test_cobs_fixed);
     RUN_TEST(test_cobs_zeros);
+    RUN_TEST(test_cobs_255);
     RUN_TEST(test_cobs_nonzeros);
 
     return UNITY_END();
